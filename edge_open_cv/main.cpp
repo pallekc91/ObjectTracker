6e8e7b19ee1c88735f7582720555487a5bdd5ed4
palle.hayagreeva@gmail.com <pallekc@Krishnas-MacBook-Pro.local>
2020-04-24 23:12:10 -0400

    adding working code to track object(specific to the local_input)

--- edge_open_cv/main.cpp
+++ edge_open_cv/main.cpp
@@ -1,52 +1,54 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include "img_utils.h"
 
 #include <iostream>
 
 using namespace cv;
-
-Mat src, src_gray,gauss_blurred,dst;
-
-//low_threshold to start filtering edges
-int low_threshold = 0;
-//maximum of low_threshold to run canny with
-int max_lowThreshold = 100;
-//kernel size to use for sobel operator
-int kernel_size = 3;
-//a name given to the window that runs over the given input image
-char* window_name = "Edge Map";
-
-void CannyThreshold(int, void*)
-{
-    blur( src_gray, gauss_blurred, Size(3,3) ); //adding gaussian blur
-    Canny( gauss_blurred, gauss_blurred, low_threshold, 3 * low_threshold, kernel_size );//running canny
-    dst = Scalar::all(0); //initialising the dst to store the output
-    src.copyTo( dst, gauss_blurred); //copying the output to dst
-    imshow( window_name, dst ); //displaying the image
-}
-
-
+RNG rng(12345);
 
 int main(){
 
-    //following code to read the image, please change the input file image location.
-    char * imgae_locaton = "/Users/pallekc/Downloads/pallekc.jpg";
-    src = imread( samples::findFile(imgae_locaton), IMREAD_COLOR );
-    if(src.empty())
-    {
-        std::cout << "Cound not find/open image" << std::endl;
-        return -1;
+    //open the video file
+    char* video_location = "/Users/pallekc/EC526/paralg_test.mp4";
+    VideoCapture cap(video_location);
+
+    //get the first frame a resize it
+    Mat frame;
+    cap >> frame;
+    resize(frame, frame, Size(512, 512), 0, 0, INTER_CUBIC);
+
+    //take the input from the user of the object that he wants to track
+    bool fromCenter = false;
+    Rect2d main_box = selectROI(frame,fromCenter);
+    cout << "x=" << main_box.x << " ,y=" << main_box.y << endl;
+    cout << "height=" << main_box.height << " ,width=" << main_box.width << endl;
+
+    //run canny on frame 1 and collect the target_output for the next frame
+    Mat canny_out = canny(frame,150,200,3);
+    Mat target_canny = canny_out(main_box);
+
+    //looping for frame by frame
+    while(1){
+        //get the next frame and resize
+        cap >> frame;
+        if (frame.empty())
+            break;
+        resize(frame, frame, Size(512, 512), 0, 0, INTER_CUBIC);
+
+        //run canny on the current frame
+        canny_out = canny(frame,150,200,3);
+
+        //find the position of the target_output in the current frame
+        Rect2d pos = lowest_loss(canny_out,target_canny);
+
+        //display the frame with a bounding box on the identified position
+        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+        rectangle( frame, pos.tl(), pos.br(), color, 10 );
+        imshow( "frame", frame );
+
+        //update the target_optput for the next frame
+        target_canny = canny_out(pos);
     }
-
-    dst.create(src.size(), src.type()); //creating the destination 
-    cvtColor(src, src_gray, COLOR_BGR2GRAY); //convering colour image to grayscale
-
-    namedWindow( window_name, WINDOW_AUTOSIZE );    //creating a window to display result
-
-    //running the "CannyThreshold" function and displaying the result in the window we just created with a low_threshold to max_lowthreshold limits
-    createTrackbar( "Min Threshold:", window_name, &low_threshold, max_lowThreshold, CannyThreshold ); 
-    CannyThreshold(0, 0);
-    waitKey(0);
-    return 0;
 }
 
--- utils/utils.h
+++
@@ -1,8 +0,0 @@
-
-double ** get_intensity_matrix(double ** x_output, double ** y_output,int image_size);
-double ** get_angle_matrix(double ** x_output, double ** y_output,int image_size);
-double ** apply_kernel_add(double ** image, double** kernel,int image_size,int kernel_size);
-double ** apply_kernel_mul(double ** image, double** kernel,int image_size,int kernel_size);
-double** gaussian_kernel(int size, double sigma);
-double ** x_sobel_kernel_3();
-double ** y_sobel_kernel_3();

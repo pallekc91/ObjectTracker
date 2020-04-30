3e648e23548d7d86839542d80a355d876c8772cc
Shubham Arora <aroras@bu.edu>
2020-04-30 20:59:05 +0000

    template acc code

--- img_utils.h
+++ img_utils.h
@@ -19,6 +19,50 @@ Mat canny(Mat input,int low_threshold, int high_threshold,int kernel_size)
 
 Rect2d lowest_loss(Mat ori, Mat target)
 {
+    Rect2d  res;
+    double min_loss = numeric_limits<double>::infinity();
+    Mat loss;
+    #pragma acc parallel loop reduction(min:min_loss)
+    for(int i=0;i<ori.cols-target.cols;i++){
+        for(int j=0;j<ori.rows-target.rows;j++){
+            Rect2d inner_mat_rect(i,j,target.cols,target.rows);
+            Mat temp = ori(inner_mat_rect);
+            Mat loss_mat;
+            absdiff(target,temp,loss_mat);
+            double loss = loss_mat.dot(loss_mat);
+            if(min_loss > loss){
+                min_loss = loss;
+            }
+        }
+    }
+    int i_min = 0;
+    int j_min = 0;
+    #pragma acc parallel loop copy(i_min,j_min)
+    for(int i=0;i<ori.cols-target.cols;i++){
+        for(int j=0;j<ori.rows-target.rows;j++){
+            Rect2d inner_mat_rect(i,j,target.cols,target.rows);
+            Mat temp = ori(inner_mat_rect);
+            Mat loss_mat;
+            absdiff(target,temp,loss_mat);
+            double loss = loss_mat.dot(loss_mat);
+            if(loss == min_loss){
+                #pragma acc atomic write
+                i_min = i;
+                #pragma acc atomic write
+                j_min = j;
+            }
+        }
+    }
+    res.x = i_min;
+    res.y = j_min;
+    res.height = target.rows;
+    res.width = target.cols;
+    cout << "returning res rect " << res << endl;
+    return res;
+}
+
+Rect2d lowest_loss_old(Mat ori, Mat target)
+{
    
     Rect2d  res;
     double min_loss = numeric_limits<double>::infinity();
